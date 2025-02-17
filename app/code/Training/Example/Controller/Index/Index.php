ddd9e9dd77400bcd90f593670928c428014587d8
Sanyam Pandey <sanyam.pandey@hbwsl.com>
2025-02-17 12:36:23 +0530

    Created Example Module and its route example/index/index

---
+++ app/code/Training/Example/Controller/Index/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\Example\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\Result\RawFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index implements ActionInterface
+{
+    protected $resultFactory;
+
+    public function __construct(RawFactory $resultFactory)
+    {
+        $this->resultFactory = $resultFactory;
+    }
+
+    public function execute(): ResultInterface
+    {
+        $result = $this->resultFactory->create();
+        $result->setContents('Example');
+        return $result;
+    }
+}
